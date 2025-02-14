c6f9c0c961f7c2f2f3cfb9263f1f08ac0beb0c42
bino1530 <nhatnam1530@gmai.com>
2025-02-15 00:31:57 +0700

    update met vl

--- js/announcement.js
+++ js/announcement.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const header = document.querySelector('.announcement_header');
+    const header = document.querySelector('.header-off');
     let lastScrollY = window.scrollY;
 
     window.addEventListener('scroll', () => {
@@ -11,3 +11,17 @@ document.addEventListener('DOMContentLoaded', () => {
         lastScrollY = window.scrollY;
     });
 });
+
+$(document).ready(function() {
+    $('.dropdown-btn > a').click(function(e) {
+      e.preventDefault();
+      $('#dropdownMenu').stop().slideToggle(500);
+    });
+  
+    $(document).click(function(event) {
+      if (!$(event.target).closest('.dropdown-btn').length) {
+        $('#dropdownMenu').slideUp(500);
+      }
+    });
+  });
+  
